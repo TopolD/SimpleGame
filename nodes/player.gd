dae324e97a228fcb9c6bf715d6cd837d9fdb4dcd
N2x4ZR <139159049+N2x4ZR@users.noreply.github.com>
2024-08-06 13:24:59 +0300

    Remove old data and add new data

---
+++ nodes/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@onready var animation = $AnimatedSprite2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	update_animation()
+	move_and_slide()
+
+func update_animation():
+	#if velocity.x < 0:
+		#animation.flip_h = true
+	#elif velocity.x > 0:
+		#animation.flip_h = false
+		
+	if velocity.x:
+		animation.play("Run")
+	else:
+		animation.play("Idle")
+		
+	if velocity.y > 0:
+		animation.play("Fall")
+	elif velocity.y < 0:
+		animation.play("Jump")
